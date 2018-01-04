0114f7ea430b6cc491f8b438523ac3a5590616d5
Homer <homerquan@gmail.com>
2018-01-04 18:07:08 -0500

    a regular commit

--- api/grpc/handlers/ListConversationsHandler.js
+++ api/grpc/handlers/ListConversationsHandler.js
@@ -2,10 +2,10 @@
 * @Author: Homer
 * @Date:   2018-01-04 00:43:43
 * @Last Modified by:   Homer
-* @Last Modified time: 2018-01-04 00:43:53
+* @Last Modified time: 2018-01-04 15:23:41
 */
 module.exports = (request,cb) => {
-	Conversations.find()
+	Conversation.find()
 		.then(items => {
 			cb(null, {data:JSON.stringify(items)});
 		})
